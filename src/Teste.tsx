2e0a3d5ea6101be1d15489910619516d82928b1d
Bruno Bessa <beduardo2014@gmail.com>
2023-04-22 10:52:10 -0300

    configurando o ambiente de desenvolvimento

---
+++ src/Teste.tsx
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+
+type BotaoProps = {
+  principal: boolean
+  fontSize?: string
+}
+
+const Botao = styled.button<BotaoProps>`
+  background-color: ${(props) => (props.principal ? 'green' : 'blue')};
+  font-size: ${(props) => props.fontSize || '16px'};
+`
+
+const BotaoPerigo = styled(Botao)`
+  background-color: red;
+  color: #fff;
+`
+
+function Teste() {
+  return (
+    <>
+      <Botao principal>Enviar</Botao>
+      <Botao fontSize="14px" principal={false}>
+        Cancelar
+      </Botao>
+      <BotaoPerigo as="a" principal>
+        Não clique aqui
+      </BotaoPerigo>
+    </>
+  )
+}
+
+export default Teste
