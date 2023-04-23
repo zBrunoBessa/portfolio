09aea4e0b9d4f6164995564fc566d8ecc1f03af8
Bruno Bessa <beduardo2014@gmail.com>
2023-04-23 10:02:51 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,4 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/zbrunobessa.png" />
+export default Avatar
--- src/components/Paragrafo/styles.ts
+++ src/components/Paragrafo/styles.ts
@@ -1,8 +1,10 @@
 import styled from 'styled-components'
 
 import { Props } from '.'
+
 export const P = styled.p<Props>`
+  font-size: ${(props) => (props.fontSize ? props.fontSize + 'px' : '14px')};
   color: ${(props) => (props.tipo === 'principal' ? '#282a35' : '#949494')};
-  font-size: 14px;
+
   line-height: 22px;
 `
